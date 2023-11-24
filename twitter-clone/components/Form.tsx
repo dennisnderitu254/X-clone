7bfec80fc3b90f3875dec221245d91b464c1e725
dennisnderitu254 <dknderitu@gmail.com>
2023-11-24 11:44:49 +0300

    Updating project README

--- twitter-clone/components/Form.tsx
+++ twitter-clone/components/Form.tsx
@@ -47,6 +47,8 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
     }
   }, [body, mutatePosts, isComment, postId, mutatePost]);
 
+
+
   return (
     <div className="border-b-[1px] border-neutral-800 px-5 py-2">
       {currentUser ? (
@@ -90,13 +92,15 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
         </div>
       ) : (
         <div className="py-8">
-          <h1 className="text-white text-2xl text-center mb-4 font-bold">Welcome to Twitter Prime</h1>
+          <h1 className="text-white text-2xl text-center mb-4 font-bold">Welcome to Twitter Prime, The Place to Be</h1>
           <div className="flex flex-row items-center justify-center gap-4">
             <Button label="Login" onClick={loginModal.onOpen} />
             <Button label="Register" onClick={registerModal.onOpen} secondary />
+
           </div>
         </div>
       )}
+
     </div>
   );
 };
--- twitter-clone/components/posts/PostFeed.tsx
+++ twitter-clone/components/posts/PostFeed.tsx
@@ -1,6 +1,7 @@
 import usePosts from '@/hooks/usePosts';
 
 import PostItem from './PostItem';
+import PostList from '../Fetchtwentyposts';
 
 interface PostFeedProps {
   userId?: string;
@@ -14,7 +15,11 @@ const PostFeed: React.FC<PostFeedProps> = ({ userId }) => {
       {posts.map((post: Record<string, any>,) => (
         <PostItem userId={userId} key={post.id} data={post} />
       ))}
+
+    <PostList />
     </>
+
+
   );
 };
 
